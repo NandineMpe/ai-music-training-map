f275a38a30f430b29f61486e5104bed0fc3519b9
NandineMpe <nandinempe@gmail.com>
2026-07-16 07:27:25 -0700

    Move Listen/Upload buttons to bottom-left

--- web/app/components/ListenButton.tsx
+++ web/app/components/ListenButton.tsx
@@ -237,7 +237,7 @@ export default function ListenButton() {
       />
 
       {/* Floating action buttons */}
-      <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-2">
+      <div className="fixed bottom-6 left-6 z-50 flex flex-col items-start gap-2">
         {/* Upload button */}
         <button
           onClick={() => fileInputRef.current?.click()}
